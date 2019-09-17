026713433d9c8cde1507ebe0cdd9d27c5068e19f
Sebastien Besson <seb.besson@gmail.com>
2019-09-17 09:46:06 +0100

    Bump to 0.4.2 development segment

--- setup.py
+++ setup.py
@@ -91,7 +91,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-version = '0.4.1'
+version = '0.4.2.dev1'
 url = "https://github.com/ome/omero-metadata/"
 
 setup(
