88f1784bf591ed2723fea7b8326d8b3fbd576385
Sebastien Besson <seb.besson@gmail.com>
2021-06-15 09:00:31 +0100

    Bump version: 0.8.1 → 0.8.2.dev0

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-version = '0.8.1'
+version = '0.8.2.dev0'
 url = "https://github.com/ome/omero-metadata/"
 
 setup(
