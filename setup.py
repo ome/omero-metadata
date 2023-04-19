f8809c647b4b09c5401c73548e6df495a373548b
Sébastien Besson <sbesson@glencoesoftware.com>
2023-04-19 08:43:04 +0100

    Bump version: 0.11.3.dev0 → 0.12.0.dev0

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-version = '0.11.3.dev0'
+version = '0.12.0.dev0'
 url = "https://github.com/ome/omero-metadata/"
 
 setup(
